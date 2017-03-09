c0329ed598a00d8673c399dc18ed3163790a6fdf
micawhitby <micawhitby@gmail.com>
2017-03-09 17:01:22 +0000

    created player constructor which has a symbol. tests passing

--- spec/PlayerSpec.js
+++ spec/PlayerSpec.js
@@ -1,14 +1,12 @@
 describe('Player', function(){
 
-  beforeEach(function(){
-    var player_1 = new Player('X');
-    var player_2 = new Player('0');
-  });
-
     it("player 1's symbol is X", function() {
+      var player_1 = new Player('X');
       expect(player_1.symbol).toBe('X')
     });
 
     it("player 2's symbol is 0", function() {
+      var player_2 = new Player('0');
       expect(player_2.symbol).toBe('0')
     });
+});
