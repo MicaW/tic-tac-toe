5bd714aa403bbcbd166bf3a383a0228abb7bdfb9
micawhitby <micawhitby@gmail.com>
2017-03-09 16:50:40 +0000

    added unit tests to check that players have a symbol.

---
+++ spec/PlayerSpec.js
@@ -0,0 +1,14 @@
+describe('Player', function(){
+
+  beforeEach(function(){
+    var player_1 = new Player('X');
+    var player_2 = new Player('0');
+  });
+
+    it("player 1's symbol is X", function() {
+      expect(player_1.symbol).toBe('X')
+    });
+
+    it("player 2's symbol is 0", function() {
+      expect(player_2.symbol).toBe('0')
+    });
